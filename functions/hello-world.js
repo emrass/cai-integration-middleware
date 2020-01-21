054b9f6d40173edaed6873d0a9d2c2c068dd5c3c
Enrico Mrass <emrass@users.noreply.github.com>
2020-01-20 21:20:45 -0800

    POST endpoint with proper text response

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -1,8 +1,29 @@
-const greeting = process.env.GREETING || "Hello World";
+const { FAQ_BOT_TOKEN } = process.env;
 
 export async function handler(event) {
+  if (event.httpMethod !== "POST") {  // Only allow POST
     return {
-      statusCode: 200,
-      body: JSON.stringify({ data: greeting })
-    }
+      statusCode: 405,
+      body: 'Method Not Allowed',
+    };
   }
+
+  console.log(event.body)
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      "replies": [{
+        "type": "text",
+        "delay": 2,
+        "content": "Hello world!",
+      }],
+      "conversation": {
+        "language": "en",
+        "memory": {
+          "user": "Bob",
+        },
+      },
+    }),
+  };
+}
